3466db71b8677670d1d12b8eb78944708045fcdf
Dustin Walker <dustinwalkerart@gmail.com>
2019-04-15 16:01:58 +0900

    adds styled-icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div``;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper className="App">
+        <div>
+          <div>Dustin Walker</div>
+          <div>Front-End Engineer and Designer</div>
+        </div>
+
+        <div>
+          <div>About Me</div>
+          <div>Front-End Engineer and Designer</div>
+        </div>
+
+        <div>
+          <div>Contact Me</div>
+          <div>Front-End Engineer and Designer</div>
+        </div>
+        <div>
+          <div>Social Links</div>
+          <div>Front-End Engineer and Designer</div>
+        </div>
+
+        <div>
+          <div>Stuff I've Made</div>
+          <div>Front-End Engineer and Designer</div>
+        </div>
+      </Wrapper>
     );
   }
 }
