45c917de26e005308a56244a715e66a43a32a55a
licebeam <dustinwalkerart@gmail.com>
2019-06-05 19:41:33 +0900

    Updates

---
+++ precache-manifest.dd4d8d91a0d2979a897520e1b9dcc4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "903e0d89b4303fe514a751067a14bce8",
+    "url": "/portfolio/static/media/tokyo.903e0d89.gif"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "794058819311db8347aa",
+    "url": "/portfolio/static/js/main.2ba0fd48.chunk.js"
+  },
+  {
+    "revision": "b2cc787664e384330b14",
+    "url": "/portfolio/static/js/2.31687c64.chunk.js"
+  },
+  {
+    "revision": "794058819311db8347aa",
+    "url": "/portfolio/static/css/main.dfd59c98.chunk.css"
+  },
+  {
+    "revision": "05210d7215b62fdc52ec6eb6ce6e3aa2",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3d91b39c6a3f7f954bd23cfe4879112a.js"
+  "/portfolio/precache-manifest.dd4d8d91a0d2979a897520e1b9dcc4f0.js"
 );
 
 workbox.clientsClaim();
